53aea4f35d1d4ca876076935d3a97dcf433e4736
AlanPeixinho <AlanPeixinho@localhost>
2013-04-08 00:49:17 -0300

    handling the app sent to background

--- src/com/example/androideye/StartupChoiceActivity.java
+++ src/com/example/androideye/StartupChoiceActivity.java
@@ -1,11 +1,15 @@
 package com.example.androideye;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Verify if it is the first time the App is running.
@@ -14,8 +18,10 @@ import android.util.Log;
  */
 public class StartupChoiceActivity extends Activity {
     
-	private static final String TAG = "StartupChoice";
+	public static final String TAG = "StartupChoice";
 	private static boolean DEBUG = false;
+	
+	private static final int CHECK_CODE = 12;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,20 +33,25 @@ public class StartupChoiceActivity extends Activity {
         	finish();
 	    	return;
         }
-
-        SharedPreferences mPrefs = getSharedPreferences("prefs", Context.MODE_PRIVATE);
         
-        if(!mPrefs.getBoolean("has_started_before", false)) {
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, CHECK_CODE);
+
+    }
+    
+    private void callStartActivity()
+    {
+        SharedPreferences mPrefs = getSharedPreferences(Globals.PersistentInfo.PREFERENCES_FILE, Context.MODE_PRIVATE);
+        
+        if(!mPrefs.getBoolean(Globals.PersistentInfo.HAS_STARTED, false)) {
             // Do what ever you want to do the first time the app is run
         	Log.v(TAG, "This is the first time the app is running.");
-            mPrefs.edit().putBoolean("has_started_before", true).commit();
 
         	startActivity(new Intent(this, Tutorial.class));
             finish();
             //Do what ever we want to do on a normal startup. This is pretty much always mean starting a new activity
         } else {
-            //Remember our choice for next time
-            mPrefs.edit().putBoolean("has_started_before", true).commit();
             Log.i(TAG, "We've already started the app at least once");
             
 
@@ -48,8 +59,28 @@ public class StartupChoiceActivity extends Activity {
             startActivity(new Intent(this, UserInterface.class));
             finish();
         }
-
     }
     
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                callStartActivity();
+            }
+            else
+            {
+            	AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage("There is an error in the Text To Speech Software.");
+                alert.show();
+                alert.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						finish();						
+					}
+				});
+            }
+        }
+    }
         
 }
